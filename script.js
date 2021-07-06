2bc96743118d34fd9c4a8456ecc6a6f785859067
Kate Rebello <katerebello27@gmail.com>
2021-07-06 18:47:53 +0530

    styles added

--- script.js
+++ script.js
@@ -1,15 +1,96 @@
-function displaynav(){
-    var navbar = document.getElementById("navbar");
-    var navbutton = document.getElementsByClassName("nav-button");
-    if(document.getElementById("navbar").style.display == 'block'){
-        navbar.style.display = 'none';
-        navbutton.style.display = "block";
-    }
-    else{
-        navbar.style.display = 'block';
-        navbutton.style.display = "none";
-    }
-}
+const projects =[
+    {
+        title: "Instgram clone",
+        image: "",
+        link: "https://github.com/katerebello/laravel_project",
+        technologies: [""],
+    },
+    {
+        title: "Electronic store",
+        image: "",
+        link: "",
+        technologies: [""],
+    },
+    {
+        title: "Music application",
+        image: "",
+        link: "",
+        technologies: [""],
+    },
+    {
+        title: "Gender predictor",
+        image: "",
+        link: "",
+        technologies: [""],
+    },
+    {
+        title: "Electronic store",
+        image: "",
+        link: "",
+        technologies: [""],
+    },
+     {
+        title: "Gender predictor",
+        image: "",
+        link: "",
+        technologies: [""],
+    },
+  
+
+];
+
+document.getElementById("projects").innerHTML = `
+         ${projects.map(function(project){
+            return `
+            <a target="_blank" href=${projects[0].link} class="project-link">
+                <div class="project-tile">
+                    <img src="https://media.istockphoto.com/vectors/programming-code-application-window-vector-id1124838925?k=6&m=1124838925&s=612x612&w=0&h=TbuwIMLPlZxN9jbuTcJanRwX33tkgNF7_PiIPvpPVm4="
+                        alt="">
+                    <div class="project-info">
+                        <h3>${project.title}</h3>
+                        <p>Laravel-php framework</p>
+                    </div>
+                    <i class="uil uil-github"></i>
+                </div>
+            </a>
+            `
+         }).join('')}  
+`
+
+const languages =["Python","PHP","Java","C++","C","Javascript","Dart","Markdown"];
+const frameworks = ["Django","Laravel","Flutter"];
+const libraries = ["Bootstrap","Jquery"];
+const technologies = ["Github","Git"];
+
+document.getElementById("languages").innerHTML = `
+    ${languages.map(function(item){
+        return `
+        <span class="lang">${item}</span>`
+    }).join('')}
+
+`
+
+document.getElementById("frameworks").innerHTML = `
+    ${frameworks.map(function(item){
+        return `
+        <span class="lang">${item}</span>`
+    }).join('')}
+
+`
+document.getElementById("libraries").innerHTML = `
+    ${libraries.map(function(item){
+        return `
+        <span class="lang">${item}</span>`
+    }).join('')}
+
+`
+document.getElementById("technologies").innerHTML = `
+    ${technologies.map(function(item){
+        return `
+        <span class="lang" >${item}</span>`
+    }).join('')}
+
+`
 
 
 
