897fd809770d242fe5ac5f437cb3ff8250928772
Kate Rebello <katerebello27@gmail.com>
2021-07-09 00:17:57 +0530

    scroll indicator

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// page scroll indicator starts
+const body = document.querySelector("body");
+const progressBar = document.querySelector("#progress-bar");
+window.addEventListener("scroll",()=>{
+    console.log("YAY");
+    let scrollValue = (window.scrollY);
+    let pageHeight = body.clientHeight -(window.innerHeight);
+    // console.log(scrollValue);
+    let progressPercentage = (scrollValue / pageHeight) * 100;
+    console.log( progressPercentage + "%");
+    document.getElementById("progress-bar").style.width = progressPercentage + "%";
+
+});
+
+// page scroll indicator end 
+
+
 const projects =[
     {
         title: "Instgram clone",
